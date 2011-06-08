d2f0ec825002ef293478f2c9e7a7ebf370b05678
Marco Bernasocchi <marco@placebo.(none)>
2011-06-08 15:10:02 +0200

    reorganizing

---
+++ scripts/compileQgisMobileLibs.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+#
+set -e
+#############################
+#######CONFIGURE HERE########
+#############################
+export ROOT_DIR=$HOME/dev/qgis-mobile
+export INSTALL_DIR=$HOME/apps/qgis-mobile
+export SRC_DIR=$ROOT_DIR/src
+export TMP_DIR=$ROOT_DIR/tmp
+export SCRIPTS_DIR=$ROOT_DIR/scripts
+export NDK=$HOME/dev/necessitas/android-ndk-r5b
+#############################
+#######END CONFIGURE#########
+#############################
+#
+
+
+ 
+usage(){
+ echo "Usage:"
+ echo " compileQgisMobileLibs.sh 
+        --install (-i)           installs the libs to INSTALL_DIR 
+        --removedownloads (-r)  removes the downloaded archives after unpacking
+        --help (-h)
+        --version (-v)
+        --echo <text> (-e)      this option does noting"
+}
+
+echo "NDK location: " $NDK
+echo "Downloading src to: " $SRC_DIR
+
+while test "$1" != "" ; do
+        case $1 in
+                --echo|-e)
+                        echo "$2"
+                        shift
+                ;;
+                --removedownloads|-r)
+                        echo "TMP_DIR and the downloaded packages will be deleted"
+                        export REMOVE_DOWNLOADS=1
+                ;;
+                --install|-i)
+                        echo "Installing libs to: " $INSTALL_DIR
+                        export INSTALL_LIBS=1
+                ;;
+                --help|-h)
+                        usage
+                        exit 0
+                ;;
+                --version|-v)
+                        echo "compileQgisMobileLibs.sh version 0.0.1"
+                        exit 0
+                ;;
+                -*)
+                        echo "Error: no such option $1"
+                        usage
+                        exit 1
+                ;;
+        esac
+        shift
+done
+
+#confirm settings
+CONTINUE=n
+echo "OK? [y, n*]:"
+read CONTINUE
+CONTINUE=$(echo $CONTINUE | tr "[:lower:]" "[:upper:]")
+if [ "$CONTINUE" = "Y" ]; then
+  continue
+else
+  echo "Abort"
+  exit 1
+fi
+
+export NDK_PLATFORM=$NDK/platforms/android-4/arch-arm
+#detect cpu cores
+export CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+mkdir -p $TMP_DIR
+#Get Updated config.sub
+wget "http://git.savannah.gnu.org/gitweb/?p=autoconf.git;a=blob_plain;f=build-aux/config.sub;hb=7420ce3483ec7d50de0667ec03b86be143f72c52" -O $TMP_DIR/config.sub
+
+#FIXME
+#diff $NDK_PLATFORM/usr/include/sys/types.h.orig $NDK_PLATFORM/usr/include/sys/types.h
+#124c124
+#< typedef uint64_t       u_int64_t;
+#---
+#
+#> //typedef uint64_t       u_int64_t;
+
+mkdir -p $SRC_DIR
+cd $SRC_DIR
+#PROJ4
+$SCRIPTS_DIR/proj-4.7.0.sh
+#END PROJ4
+cd $SRC_DIR
+#GEOS3.2.2
+$SCRIPTS_DIR/geos-3.2.2.sh
+#END GEOS3.2.2
+
+if [ $REMOVE_DOWNLOADS -eq 1 ] ; then rm -rf $TMP_DIR; fi
+exit 0
--- scripts/proj-4.7.0.sh
+++ scripts/proj-4.7.0.sh
@@ -1,10 +1,11 @@
 #PROJ4
 wget http://download.osgeo.org/proj/proj-4.7.0.tar.gz
 tar xf proj-4.7.0.tar.gz
-#rm proj-4.7.0.tar.gz
+if [ $REMOVE_DOWNLOADS -eq 1 ] ; then rm proj-4.7.0.tar.gz; fi
 cd proj-4.7.0/
 cp -f $TMP_DIR/config.sub ./config.sub
 
+#SET compile flags
 CXX=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-g++ \
 CXXFLAGS="-nostdlib -I$NDK_PLATFORM/usr/include -I$NDK/sources/cxx-stl/gnu-libstdc++/include -I$NDK/sources/cxx-stl/gnu-libstdc++/libs/armeabi/include -DHAVE_ISNAN" \
 CC=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-gcc \
@@ -12,5 +13,13 @@ CFLAGS="  -nostdlib -I$NDK_PLATFORM/usr/include -I$NDK/sources/cxx-stl/gnu-libst
 LDFLAGS="-L$NDK/sources/cxx-stl/gnu-libstdc++/libs/armeabi -L$NDK_PLATFORM/usr/lib --sysroot=$NDK_PLATFORM -Wl,-rpath-link=$NDK_PLATFORM/usr/lib -lc" \
 ./configure --prefix=$INSTALL_DIR --host=arm-linux-androideabi
 
+#COMPILE LIB
 make -j$CORES 2>&1 | tee make.out
+
+#INSTALL LIB
+if [ $INSTALL_LIBS -eq 1 ]
+  then make -j$CORES 2>&1 install | tee makeInstall.out
+fi
+
+exit 0
 #END PROJ4
