79fe3a09fcdc19842b3cc3fa5d28c06427869a36
Marco Bernasocchi <marco@bernawebdesign.ch>
2011-08-08 05:58:35 +0200

    added test-apk project

---
+++ scripts/install-apk.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#   ***************************************************************************
+#     install-apk.sh - builds the and installs the needed libraries for android QGIS
+#      --------------------------------------
+#      Date                 : 01-Aug-2011
+#      Copyright            : (C) 2011 by Marco Bernasocchi
+#      Email                : marco at bernawebdesign.ch
+#   ***************************************************************************
+#   *                                                                         *
+#   *   This program is free software; you can redistribute it and/or modify  *
+#   *   it under the terms of the GNU General Public License as published by  *
+#   *   the Free Software Foundation; either version 2 of the License, or     *
+#   *   (at your option) any later version.                                   *
+#   *                                                                         *
+#   ***************************************************************************/
+
+set -e
+
+source `dirname $0`/config.conf
+
+
+cd $APK_DIR
+ant install
+adb -d shell am start -n org.qgis.qgis/eu.licentia.necessitas.industrius.QtActivity
+
