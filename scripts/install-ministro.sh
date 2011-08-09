0e2b901bbc2e4a393ac490a781506b2f5426afbf
Marco Bernasocchi <marco@bernawebdesign.ch>
2011-08-09 04:54:55 +0200

    added explanation

--- scripts/install-ministro.sh
+++ scripts/install-ministro.sh
@@ -22,5 +22,8 @@ source `dirname $0`/config.conf
 cd $TMP_DIR
 wget -c -O MinistroConfigurationTool-1.0.apk http://downloads.sourceforge.net/project/ministro.necessitas.p/MinistroConfigurationTool-1.0.apk?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fministro.necessitas.p%2Ffiles%2FMinistroConfigurationTool-1.0.apk%2Fdownload%3Fuse_mirror%3Dkent&ts=1312857470&use_mirror=garr
 wget -c -O Ministro-2.0.apk http://downloads.sourceforge.net/project/ministro.necessitas.p/Ministro-2.0.apk?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fministro.necessitas.p%2Ffiles%2F&ts=1312857514&use_mirror=heanet
+#installs ministro on the only running emulator.
+#returns an error if more than one emulator is running.
+#real devices will find ministro on the market
 adb -e install MinistroConfigurationTool-1.0.apk
 adb -e install Ministro-2.0.apk 
