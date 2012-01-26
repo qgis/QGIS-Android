8639f6f9812eff4dc32cbb4812dc76662c669604
Marco Bernasocchi <marco@bernawebdesign.ch>
2012-01-26 01:34:42 +0100

    adding reinstall on device script

---
+++ scripts/reinstall-apk.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#   ***************************************************************************
+#     build-apk.sh - builds the and installs the needed libraries for android QGIS
+#      --------------------------------------
+#      Date                 : 01-Aug-2011
+#      Copyright            : (C) 2011 by Marco Bernasocchi
+#      Email                : marco at bernawebdesign.ch
+#   ***************************************************************************
+#   *                                                                         *
+#   *   This program is free software; you can redistribute it and/or modify  *
+#   *   it under the terms of the GNU General Public License as published by  *
+#   *   the Free Software Foundation; either version 2 of the License, or     *
+#   *   (at your option) any later version.                                   *
+#   *                                                                         *
+#   ***************************************************************************/
+
+set -e
+
+source `dirname $0`/config.conf
+ADB=$ANDROID_SDK_ROOT/platform-tools/adb
+$ADB kill-server
+sudo $ADB devices
+
+echo "Installing $APK_DIR/bin/qgis-debug.apk"
+$ADB -d install -r $APK_DIR/bin/qgis-debug.apk
