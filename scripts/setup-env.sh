703cc9a21b519aad7af3fb64d6c7a5a4bd7efd98
Marco Bernasocchi <marco@opengis.ch>
2013-09-06 18:19:15 +0200

    added support for custom qgis branch name

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -135,12 +135,12 @@ else
   
   #check if an android branch of qgis is present
   set +e
-    git checkout android
+    git checkout $QGIS_ANDROID_BRANCH
   set -e
   BRANCH="$(git branch 2>/dev/null | sed -e "/^\s/d" -e "s/^\*\s//")"
   
-  if [ "$BRANCH" != "android" ]; then
-    echo "Aborting, the qgis branch checkedout is not 'android', please clone or fork this repo: git://github.com/mbernasocchi/Quantum-GIS.git"
+  if [ "$BRANCH" != "$QGIS_ANDROID_BRANCH" ]; then
+    echo "Aborting, the qgis branch checkedout is not '$QGIS_ANDROID_BRANCH', please clone or fork this repo: git://github.com/mbernasocchi/Quantum-GIS.git"
     exit 1
   else
     echo "Environement looks good, lets start"
